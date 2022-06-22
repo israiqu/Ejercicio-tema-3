44ab9e7b7011d495573e7b073141d5db7e171bc5
isra.dev@outlook.com <isra.dev@outlook.com>
2022-06-22 03:24:14 -0500

    Commit final

--- src/App.java
+++ src/App.java
@@ -1,5 +1,31 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    // Primera parte        
+        int a;
+        int b;
+        int c;
+
+        int res = add(a = 1985, b = 1597, c = 1785);
+
+        System.out.println("El resultado de la suma es " + res);
+
+    // Segunda parte
+        Coche miCoche = new Coche();
+        miCoche.addPuerta();
+        System.out.println("El coche tiene " + miCoche.puertas + " puertas");
+    }
+
+    // Primera parte
+    public static int add(int a, int b, int c) {
+        return a + b + c;
+    }
+}
+
+    // Segunda parte
+class Coche {
+    public int puertas = 2;
+
+    public void addPuerta() {
+        this.puertas++;
     }
 }
